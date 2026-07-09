feea5145fefaf7a5d7321fea9b9ed7871b4234fe
Aryan Sharma <aryanstark3000@gmail.com>
2026-07-09 16:20:14 +0530

    refactor(config): support wrapping root cause in ConfigurationValidationException

--- src/main/java/com/julius/clipper/config/validation/ConfigurationValidationException.java
+++ src/main/java/com/julius/clipper/config/validation/ConfigurationValidationException.java
@@ -1,7 +1,12 @@
 package com.julius.clipper.config.validation;
 
 public class ConfigurationValidationException extends RuntimeException {
+    
     public ConfigurationValidationException(String message) {
         super(message);
+    }
+
+    public ConfigurationValidationException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
